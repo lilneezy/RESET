b40813ee675ed323450b1e5d0ed8b1d9cd53a12a
nihalbo <nihal.boina@gmail.com>
2021-04-29 01:41:23 -0400

    verysexc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Home from './Components/Home';
+import Videos from './Components/Videos';
+import ContactUs from './Components/ContactUs';
+import AboutUs from './Components/AboutUs';
+
+class App extends Component {
+  render(){
+    return (
+    <div className="wrapper">
+      <Navbar bg="dark" variant="dark">
+        <div class="container">
+          <Navbar.Brand href="/Home">Home</Navbar.Brand>
+          <Navbar.Brand href="/Videos">Videos</Navbar.Brand>
+          <Navbar.Brand href="/AboutUs">About Us</Navbar.Brand>
+          <Navbar.Brand href="/ContactUs">Contact Us</Navbar.Brand>
+        </div>
+      </Navbar>
+      <BrowserRouter>
+        <Switch>
+        <Route path="/Home">
+            <Home />
+          </Route>
+          <Route path="/Videos">
+            <Videos />
+          </Route>
+          <Route path="/ContactUs">
+            <ContactUs />
+          </Route>
+          <Route path="/AboutUs">
+            <AboutUs />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+    );
+    }
 }
 
 export default App;
---
+++ src/Components/Videos.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Videos() {
+  return <h2>Videos!</h2>;
+}
